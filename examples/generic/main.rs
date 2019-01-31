921538824517099ae7c2a7406d6365fe984b4eb6
Cormac Relf <web@cormacrelf.net>
2019-01-31 19:29:02 +1100

    use a Carrier trait in the generics exampl instead of re-specifying bounds

--- examples/generic/main.rs
+++ examples/generic/main.rs
@@ -2,38 +2,42 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::sync::Arc;
 
-#[salsa::query_group(HelloWorldStorage)]
-trait HelloWorld<Generic>: salsa::Database {
-    #[salsa::input]
-    fn input_yo(&self, key: ()) -> Arc<Generic>;
-    fn yo(&self, key: ()) -> Arc<Generic>;
+trait Carrier: Default + Debug + Clone + Eq + Hash + Send + Sync + 'static {
+    type Assoc: Default + Debug + Clone + Eq + Hash + Send + Sync + 'static;
+}
+#[derive(Default, Debug, Clone, PartialEq, Eq, Hash)]
+struct OptStringCarrier;
+impl Carrier for OptStringCarrier {
+    type Assoc = Option<String>;
 }
 
-fn yo<T: Default + Hash + Debug + Eq + Clone + Sync + Send + Sized + 'static>(
-    db: &impl HelloWorld<T>,
-    _key: (),
-) -> Arc<T> {
-    db.input_yo(()).clone()
+#[salsa::query_group(HelloWorldStorage)]
+trait HelloWorld<T: Carrier>: salsa::Database {
+    #[salsa::input]
+    fn input_yo(&self) -> Arc<T::Assoc>;
+    fn yo(&self) -> Arc<T::Assoc>;
+}
+
+fn yo<T: Carrier>(db: &impl HelloWorld<T>) -> Arc<T::Assoc> {
+    db.input_yo().clone()
 }
 
 #[salsa::database(HelloWorldStorage<T>)]
 #[derive(Default)]
-struct DatabaseStruct<T: Debug + Hash + Default + Clone + Eq + PartialEq + Send + Sync + 'static> {
+struct DatabaseStruct<T: Carrier> {
     runtime: salsa::Runtime<DatabaseStruct<T>>,
 }
 
-impl<T: Debug + Hash + Default + Eq + PartialEq + Clone + Send + Sync + 'static> salsa::Database
-    for DatabaseStruct<T>
-{
+impl<T: Carrier> salsa::Database for DatabaseStruct<T> {
     fn salsa_runtime(&self) -> &salsa::Runtime<DatabaseStruct<T>> {
         &self.runtime
     }
 }
 
 fn main() {
-    let mut db: DatabaseStruct<Option<String>> = DatabaseStruct::default();
+    let mut db: DatabaseStruct<OptStringCarrier> = DatabaseStruct::default();
 
-    db.set_input_yo((), Arc::new(Some("we used Option<String>".into())));
+    db.set_input_yo(Arc::new(Some("we used Option<String>".into())));
 
-    println!("{:?}.", db.yo(()));
+    println!("{:?}.", db.yo());
 }
